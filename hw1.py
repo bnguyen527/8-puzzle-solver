62d47620565c9869a5333ddc79762067f6b9c5fd
Binh Nguyen <tug21976@temple.edu>
2019-01-31 20:47:55 -0500

    Add solvability check

--- hw1.py
+++ hw1.py
@@ -1,8 +1,10 @@
+from itertools import chain
 import math
 
 
 class Puzzle:
     def __init__(self, puzzle):
+        self.state_flat = puzzle
         self.width = int(math.sqrt(len(puzzle)))
         self.state = reshape(puzzle, self.width)
 
@@ -13,10 +15,29 @@ class Puzzle:
                 return i, j
             except ValueError:
                 continue
+    
+    def is_solvable(self):
+        if self.width % 2 != 0 and self.count_inversions() % 2 != 0:
+            return False
+        return True
+            
+    def count_inversions(self):
+        state = self.state_flat
+        count = 0
+        for i in range(self.width**2):
+            if state[i] != 0:
+                for j in range(i+1, self.width**2):
+                    if state[j] != 0 and state[i] > state[j]:
+                        count += 1
+        return count
 
 
 def reshape(array, width):
     return [array[i:i+width] for i in range(0, width**2, width)]
+
+
+def flatten(array_2d):
+    return list(chain.from_iterable(array_2d))
 
 
 def main():
@@ -24,7 +45,7 @@ def main():
         puzzle_input = f.readline()
     puzzle_input = list(map(int, puzzle_input[:-1].split(',')))
     puzzle = Puzzle(puzzle_input)
-    print(puzzle.find(0))
+    print(puzzle.is_solvable())
 
 
 if __name__ == '__main__':
