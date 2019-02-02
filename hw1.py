17c0cf08eca834fe304f5c09046e11b109b5b323
Binh Nguyen <tug21976@temple.edu>
2019-02-01 21:22:28 -0500

    Fix format according to PyLint

--- hw1.py
+++ hw1.py
@@ -14,7 +14,7 @@ class Solver:
 
     Attributes:
         goal_state_1 (list), goal_state_2 (list): Goal states for heuristic calculations only.
-    
+
     Args:
         start_state (Puzzle): Start state.
 
@@ -22,52 +22,58 @@ class Solver:
 
     goal_state_1 = None
     goal_state_2 = None
-    
+
     def __init__(self, start_state):
         self.start_state = start_state
         self.frontier = []  # Visited nodes.
         self.expanded = set()   # Expanded nodes.
         # Order of insertion into frontier, tie-breaks for nodes with same priorities, especially with uniform cost search.
         self.num_visited = 0
-    
+
     # Solve the puzzle given by the start state using the heuristic provided.
     def solve(self, heuristic):
         root = Node(self.start_state, None, 0, [])
         if not root.state.is_solvable():    # Unsolvable.
             return None
-        if heuristic != Solver.uniform_cost:    # Goal states only needed for heuristics.
+        # Goal states only needed for heuristics.
+        if heuristic != Solver.uniform_cost:
             tiles = list(range(1, root.state.width**2))
             Solver.goal_state_1 = [0] + tiles
             Solver.goal_state_2 = tiles + [0]
         self.num_visited += 1
-        heappush(self.frontier, (root.depth + heuristic(root), self.num_visited, root))
+        heappush(self.frontier, (root.depth +
+                                 heuristic(root), self.num_visited, root))
         while self.frontier:
             cur_node = heappop(self.frontier)[-1]
             if cur_node.state.is_goal_state():  # Solved!
                 solution = self.solved(heuristic, cur_node)
                 return solution
-            cur_position = tuple(cur_node.state.position_flat)  # Tuples are hashable.
-            if cur_position not in self.expanded:   # Avoid expanding nodes already expanded.
+            # Tuples are hashable.
+            cur_position = tuple(cur_node.state.position_flat)
+            # Avoid expanding nodes already expanded.
+            if cur_position not in self.expanded:
                 self.expanded.add(cur_position)
                 for direction in cur_node.state.possible_moves():
                     new_node = copy.deepcopy(cur_node)
                     new_node.state.move_blank(direction)    # Expansion.
-                    if tuple(new_node.state.position_flat) not in self.expanded:    # Only visit nodes not expanded yet.
+                    # Only visit nodes not expanded yet.
+                    if tuple(new_node.state.position_flat) not in self.expanded:
                         self.num_visited += 1
                         new_node.parent = cur_node  # Add into the search tree.
                         new_node.depth += 1
                         new_node.path.append(direction)
-                        heappush(self.frontier, (new_node.depth + heuristic(new_node), self.num_visited, new_node))
-    
+                        heappush(self.frontier, (new_node.depth +
+                                                 heuristic(new_node), self.num_visited, new_node))
+
     # Return a Solution object populated with information about the solution.
     def solved(self, heuristic, goal_node):
         return Solution(self.start_state, heuristic, len(self.expanded), goal_node.state, goal_node.path)
-    
+
     # Return heuristic for uniform cost search.
     @staticmethod
     def uniform_cost(node):
         return 0
-    
+
     # Return misplaced tiles heuristic.
     @staticmethod
     def tiles(node):
@@ -78,13 +84,13 @@ class Solver:
     @staticmethod
     def misplaced_tiles(node, goal_state):
         return compare_arrays(node.state.position_flat, goal_state)
-    
+
     # Return Manhattan distance heuristic.
     @staticmethod
     def manhattan(node):
         # Take the minimum of the forward cost to either of the goal states to make sure heuristic is admissible.
         return min(Solver.manhattan_heuristic(node, Solver.goal_state_1), Solver.manhattan_heuristic(node, Solver.goal_state_2))
-        
+
     # Return Manhattan distance heuristic with regard to a specifc goal state.
     @staticmethod
     def manhattan_heuristic(node, goal_state):
@@ -121,18 +127,20 @@ class Solution():
         self.num_expanded = num_expanded
         self.sol_state = sol_state
         self.sol_path = sol_path
-    
+
     def __str__(self):
         # Pretty display of the solution.
         solution_str = []
         solution_str.append('\nHeuristic: {}'.format(self.heuristic.__name__))
-        solution_str.append('Number of expanded nodes: {}'.format(self.num_expanded))
+        solution_str.append(
+            'Number of expanded nodes: {}'.format(self.num_expanded))
         solution_str.append('\nStart state:')
         solution_str.append('\n{}\n'.format(self.start_state))
         solution_str.append('Width: n = {}'.format(self.start_state.width))
         solution_str.append('Solution state:')
         solution_str.append('\n{}\n'.format(self.sol_state))
-        solution_str.append('Solution path depth: {}'.format(len(self.sol_path)))
+        solution_str.append(
+            'Solution path depth: {}'.format(len(self.sol_path)))
         solution_str.append('Solution path:')
         solution_str.append(','.join(self.sol_path))
         return '\n'.join(solution_str)
@@ -156,7 +164,7 @@ class Node:
         self.path = path
 
 
-class Puzzle:  
+class Puzzle:
     """Represent a n-puzzle.
 
     Args:
@@ -167,16 +175,18 @@ class Puzzle:
     def __init__(self, position):
         self.position_flat = position
         self.width = int(math.sqrt(len(position)))
-        self.position = reshape(position, self.width)   # 2D representation of the puzzle.
-        self.blank_ix = self.find(0)    # Indices of blank tile in 2D representation.
+        # 2D representation of the puzzle.
+        self.position = reshape(position, self.width)
+        # Indices of blank tile in 2D representation.
+        self.blank_ix = self.find(0)
 
     def __str__(self):
-        # Return pretty 2D representation of the puzzle 
+        # Return pretty 2D representation of the puzzle
         return '\n'.join([' '.join(list(map(str, row))) for row in self.position]).replace('0', ' ')
 
     # Find coordinates of blank tile in 2D representation of the puzzle.
     def find(self, tile):
-        for i in range(self.width): # For each row.
+        for i in range(self.width):  # For each row.
             try:
                 j = self.position[i].index(tile)    # Find column index in row.
                 return i, j
@@ -191,11 +201,13 @@ class Puzzle:
 
     # Count the number of inversions in the puzzle. An inversion is when a bigger-numbered tile appears before a smaller-numbered tile.
     def count_inversions(self):
-        position = self.position_flat   # Use flat representation for convenience.
+        # Use flat representation for convenience.
+        position = self.position_flat
         count = 0
         for i in range(self.width**2):
             if position[i] != 0:    # Blank tile doesn't count in inversions.
-                for j in range(i+1, self.width**2): # Traverse through the tiles which appear after this tile.
+                # Traverse through the tiles which appear after this tile.
+                for j in range(i+1, self.width**2):
                     if position[j] != 0 and position[i] > position[j]:
                         count += 1
         return count
@@ -230,8 +242,9 @@ class Puzzle:
             swap_elements(self.position, self.blank_ix, (row, col+1))
             col += 1
         self.blank_ix = (row, col)  # Update the blank tile's position.
-        self.position_flat = flatten(self.position) # Update the flat representation of the puzzle.
-    
+        # Update the flat representation of the puzzle.
+        self.position_flat = flatten(self.position)
+
     # Transform the current puzzle using the specified move sequence. Mostly for debugging and verification.
     def move_sequence(self, sequence, verbose=False):
         if verbose:
@@ -248,7 +261,8 @@ class Puzzle:
     # Check if the position is a goal state.
     def is_goal_state(self):
         state = self.position_flat  # Use flat representation for convenience.
-        if state[0] != 0 and state[-1] != 0:    # Goal states always have blank tile at the first or last position.
+        # Goal states always have blank tile at the first or last position.
+        if state[0] != 0 and state[-1] != 0:
             return False
         for i in range(self.width**2 - 1):
             if state[i+1] != 0 and state[i] > state[i+1]:
@@ -282,7 +296,8 @@ def swap_elements(array_2d, ix1, ix2):
 
 # Count the differences in two 1D arrays.
 def compare_arrays(array_1, array_2):
-    if len(array_1) != len(array_2):    # Does not work with arrays of different lengths.
+    # Does not work with arrays of different lengths.
+    if len(array_1) != len(array_2):
         return -1
     return sum(1 if array_1[i] != array_2[i] else 0 for i in range(len(array_1)))
 
@@ -311,8 +326,10 @@ def solve_n_times(width, heuristics, num_instances):
                 print('Heuristic: {}'.format(heuristic.__name__))
                 solver = Solver(start_state)
                 solution = solver.solve(heuristic)
-                num_expanded_dict[heuristic.__name__].append(solution.num_expanded)
-            sol_depths.append(len(solution.sol_path))   # Same depth for all heuristics due to optimality.
+                num_expanded_dict[heuristic.__name__].append(
+                    solution.num_expanded)
+            # Same depth for all heuristics due to optimality.
+            sol_depths.append(len(solution.sol_path))
             num_instances -= 1
     return sol_depths, num_expanded_dict
 
@@ -325,7 +342,8 @@ def plot_num_expanded_dict(sol_depths, num_expanded_dict):
     plt.ylabel('Number of Nodes Expanded')
     colors = ['b', 'g', 'r']
     for heuristic, color in zip(num_expanded_dict.keys(), colors[:len(num_expanded_dict)]):
-        plt.plot(sol_depths, num_expanded_dict[heuristic], '{}.'.format(color), label=heuristic)
+        plt.plot(sol_depths, num_expanded_dict[heuristic], '{}.'.format(
+            color), label=heuristic)
     plt.legend(loc='best')
     plt.savefig('algorithm_efficiency.png', bbox_inches='tight')
     plt.show()
@@ -335,11 +353,13 @@ def main():
     if len(sys.argv) < 2:
         err_msg = 'Missing argument! Syntax: python hw1.py input_file [heuristic] or python hw1.py -r [num_instances]'
         sys.exit(err_msg)
-    if sys.argv[1] == '-r' and len(sys.argv) == 3:  # Mass solving of random puzzles.
+    # Mass solving of random puzzles.
+    if sys.argv[1] == '-r' and len(sys.argv) == 3:
         num_instances = int(sys.argv[2])
         print('Solving randomly generated {} 8-puzzles and plotting results...'.format(num_instances))
         heuristics = [Solver.uniform_cost, Solver.tiles, Solver.manhattan]
-        sol_depths, num_expanded_dict = solve_n_times(3, heuristics, num_instances)
+        sol_depths, num_expanded_dict = solve_n_times(
+            3, heuristics, num_instances)
         # Pickle the results for use later.
         with open('sol_depths.pkl', 'wb') as f:
             pickle.dump(sol_depths, f)
@@ -350,19 +370,21 @@ def main():
         print(num_expanded_dict)
         plot_num_expanded_dict(sol_depths, num_expanded_dict)
     else:
-        if sys.argv[1] == '-r': # Solve just 1 random puzzle.
+        if sys.argv[1] == '-r':  # Solve just 1 random puzzle.
             print('Solving randomly generated 8-puzzle...')
             start_state = Puzzle.generate(3)
             heuristics = [Solver.uniform_cost, Solver.tiles, Solver.manhattan]
         else:
-            if len(sys.argv) < 3 or sys.argv[2] == 'manhattan': # Default heuristic is Manhattan distance.
+            # Default heuristic is Manhattan distance.
+            if len(sys.argv) < 3 or sys.argv[2] == 'manhattan':
                 heuristics = [Solver.manhattan]
             elif sys.argv[2] == 'ucs':
                 heuristics = [Solver.uniform_cost]
             elif sys.argv[2] == 'tiles':
                 heuristics = [Solver.tiles]
             elif sys.argv[2] == 'all':
-                heuristics = [Solver.uniform_cost, Solver.tiles, Solver.manhattan]
+                heuristics = [Solver.uniform_cost,
+                              Solver.tiles, Solver.manhattan]
             else:
                 err_msg = "Invalid heuristic! Please select either 'ucs', 'tiles, 'manhattan', or 'all'."
                 sys.exit(err_msg)
